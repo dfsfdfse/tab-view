57353749c7830867fcb5f04b94ad59b829be7743
dfsfdfse <547367379@qq.com>
2020-08-29 16:42:15 +0800

    fix(解决滑动因事件冒泡问题出现的滚动异常bug)

--- tab-view-item/tab-view-item.js
+++ tab-view-item/tab-view-item.js
@@ -4,40 +4,66 @@ Component({
     "../tab-view/tab-view":{
       type:'parent',
       linked(target) {
-        this.setData({parentTarget:target})
+        this.setData({parentTarget:target});
       }
     }
   },
   lifetimes:{
     ready() {
-        let {scrollContainerWidth} = this.data.parentTarget.data;
-        this.setData({scrollContainerWidth:scrollContainerWidth});
+      this.createSelectorQuery().select('.tab-view-item-default').boundingClientRect(res=>{
+        let {scrollContainerWidth,containerHeight} = this.data.parentTarget.data;
+        this.setData({scrollContainerWidth:scrollContainerWidth,selfContainerHeight:containerHeight-res.height});
+      }).exec();
     }
+  },
+  pageLifetimes:{
+    
   },
   /**
    * 组件的属性列表
    */
   properties: {
+    title:{type:String,value:'未配置'},
+    itemClass:{
+      type:String
+    },
     scrollContainerWidth:{
-      type:Number
+      type:String
+    },
+    scrollContainerHeight:{
+      type:String
+    },
+    isFlex:{
+      type:Boolean,
+      value:false
+    },
+    canScroll:{
+      type:Boolean,
+      value:true,
+    },
+    canScrollX:{
+      type:Boolean,
+      value:false
     }
   },
   /**
    * 组件的初始数据
    */
   data: {
-    parentTarget:{}
+    parentTarget:{},
+    selfContainerHeight:0
   },
-
+  options:{
+    addGlobalClass:true
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-    changeCurrent(e){
-      console.log(e);
-    },
-    getComponentWidth(e){
-      console.log(e);
+    itemScroll(e){
+      let {childCanScroll} = this.data.parentTarget.data;
+      this.triggerEvent('scroll',e);
+      this.setData({canScroll:childCanScroll});
     }
   }
 })
